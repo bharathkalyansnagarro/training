73678feccb960cd02f36aef0eb94e0d4ef985942
bharathkalyan <bharath.kalyan@nagarro.com>
2022-10-17 16:26:10 +0530

    Added try and block statements for InputMisMatch Exception!

--- Core Java Assignment/src/CalculateInsurance.java
+++ Core Java Assignment/src/CalculateInsurance.java
@@ -1,31 +1,45 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalculateInsurance {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean enterLoop = true;
+        while (enterLoop) {
+            try {
+                System.out.println("Enter Car Model : ");
+                String carModel = scanner.next();
 
-        while (true) {
-            System.out.println("Enter Car Model : ");
-            String carModel = scanner.next();
+                System.out.println("Enter Car Cost Price : ");
+                long carCost = scanner.nextLong();
 
-            System.out.println("Enter Car Cost Price : ");
-            long carCost = scanner.nextLong();
+                System.out.println("Please Enter Car Type!");
+                System.out.println("Available Car Types : \n1.HatchBack\n2.Sedan\n3.SUV");
+                int carType = scanner.nextInt();
+                while (carType < 0 || carType > 3) {
+                    System.out.println("Please choose from above options!");
+                    carType = scanner.nextInt();
+                }
 
-            System.out.println("Please Enter Car Type!");
-            System.out.println("Available Car Types : \n1.HatchBack\n2.Sedan\n3.SUV");
-            int carType = scanner.nextInt();
+                System.out.println("Please Enter Type of Insurance ");
+                System.out.println("Available Insurance Types : \n1.Basic \n2.Premium");
+                int insuranceType = scanner.nextInt();
+                while (insuranceType < 0 || insuranceType > 2) {
+                    System.out.println("Please choose from above options!");
+                    insuranceType = scanner.nextInt();
+                }
 
+                long calculatedInsurance = calculateInsurance(carCost, carType, insuranceType);
+                System.out.println("Insurance to be paid :: " + calculatedInsurance + ", car model : " + carModel);
 
-            System.out.println("Please Enter Type of Insurance ");
-            System.out.println("Available Insurance Types : \n1.Basic \n2.Premium");
-            int insuranceType = scanner.nextInt();
-
-            long calculatedInsurance = calculateInsurance(carCost, carType, insuranceType);
-            System.out.println("Insurance to be paid :: " + calculatedInsurance);
-
-            System.out.println("Do you want to enter details of any other car (y/n):");
-            char reply = scanner.next().charAt(0);
-            if (reply == 'n') break;
+                System.out.println("Do you want to enter details of any other car (y/n):");
+                char reply = scanner.next().charAt(0);
+                if (reply != 'y') enterLoop = false;
+            }
+            catch (InputMismatchException exception){
+                System.out.println("Entered Wrong Input!");
+                break;
+            }
         }
     }
 
