76858111dffc4db3f4c58778a07964ef838bf323
bharathkalyan <bharath.kalyan@nagarro.com>
2022-10-20 12:39:11 +0530

    Implemented Selection Sort.

--- DataStructuresAndAlgorithms/src/Main.java
+++ DataStructuresAndAlgorithms/src/Main.java
@@ -1,11 +1,11 @@
-import datastructures.BubbleSort;
+import datastructures.SortingAlgorithms;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = new int[]{2, 1, 5, 4, 3};
-        BubbleSort object = new BubbleSort();
-        object.sort(array);
+        int[] array = new int[]{2, 1, 5, 4, 3, -1, 0};
+        SortingAlgorithms object = new SortingAlgorithms();
+        object.selectionSort(array);
         object.printArray(array);
     }
 
