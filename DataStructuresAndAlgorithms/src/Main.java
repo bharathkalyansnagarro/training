535694ada76c7455b3371ce950a016decf9f0dd5
bharathkalyan <bharath.kalyan@nagarro.com>
2022-10-28 16:32:50 +0530

    Built Jar for the Drivers!

--- DataStructuresAndAlgorithms/src/Main.java
+++ DataStructuresAndAlgorithms/src/Main.java
@@ -1,21 +1,8 @@
-import datastructures.CustomHashMap;
-import datastructures.CustomQueue;
-import datastructures.CustomStack;
-
-import java.util.Iterator;
 
 public class Main {
 
     public static void main(String[] args) {
-        CustomQueue<Integer> q = new CustomQueue<>();
-        q.enqueue(12);
-        q.enqueue(13);
-        q.enqueue(14);
-        q.enqueue(15);
-        q.enqueue(-15);
-        q.enqueue(19995);
-        q.print();
-        System.out.println(q.dequeue());
+
     }
 
 
