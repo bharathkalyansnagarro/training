f298bdeeba86516b0de26a78e34844e7b0ebbb2d
bharathkalyan <bharath.kalyan@nagarro.com>
2023-04-17 13:15:23 +0530

    Setting Up of Routes

---
+++ blog-app/src/components/DetailsPost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DetailsPost = () => {
+  return (
+    <div>DetailsPost</div>
+  )
+}
+
+export default DetailsPost
--- blog-app/src/components/Home.jsx
+++ blog-app/src/components/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Post from './Post'
+import AddPost from './AddPost'
+import EditPost from './EditPost'
+import DetailsPost from './DetailsPost'
+
+
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className="flex flex-col flex-grow m-4 p-4 border-x-4 border-y-2 rounded-lg border-yellow-300">
+            <Routes>
+                <Route path="" element={<Post />}></Route>
+                <Route path="addPost" element={<AddPost />}></Route>
+                <Route path="editPost" element={<EditPost />}></Route>
+                <Route path="detailsPost" element={<DetailsPost />}></Route>
+            </Routes>
+        </div>
     )
 }
 
